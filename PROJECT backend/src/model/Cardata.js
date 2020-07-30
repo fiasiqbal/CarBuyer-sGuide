ab5c3bf4add2daebea4def00cf21a531cd979f3d
Fias <fiasiqbal@gmail.com>
2020-07-30 17:40:07 +0530

    Database shifted to MongoDB Atlas

--- PROJECT backend/src/model/Cardata.js
+++ PROJECT backend/src/model/Cardata.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CarsDb');
+// mongoose.connect('mongodb://localhost:27017/CarsDb');
+mongoose.connect('mongodb+srv://user_fias:30031995@mycluster.qm3db.azure.mongodb.net/CarsDb?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
 const CarSchema = new Schema({
